4d4a25b24de717c3d8cdaeb0e2e714ff84226948
YunChienLou <47598862+YunChienLou@users.noreply.github.com>
2022-06-09 14:45:44 +0800

    batch file

---
+++ builder.bat
@@ -0,0 +1,39 @@
+echo off
+:::
+:::
+:::
+:::______                    _                             _____ ___________  _____ 
+:::| ___ \                  | |                    ____   |_   _|_   _| ___ \/  __ \
+:::| |_/ /   _  __ _ _ __   | |     ___  _   _    / __ \    | |   | | | |_/ /| /  \/
+:::|    / | | |/ _` | '_ \  | |    / _ \| | | |  / / _` |   | |   | | |    / | |    
+:::| |\ \ |_| | (_| | | | | | |___| (_) | |_| | | | (_| |   | |  _| |_| |\ \ | \__/\
+:::\_| \_\__, |\__,_|_| |_| \_____/\___/ \__,_|  \ \__,_|   \_/  \___/\_| \_| \____/
+:::       __/ |         ______                    \____/                            
+:::      |___/         |______|                                                     
+:::                                      
+:::                                              
+:::
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
+@REM 檢查專案外面有沒有PAGINATION
+echo Hello !! Starting deploy process at %time%
+echo running at %cd%
+echo.
+if exist ../pagination-example  (
+    echo pagination-example exist , start building...
+    cd ../pagination-example
+    npm install
+    npm run build
+    if exist dist (
+        del /S /Q "../JPA_Pagination/src/main/resources/static"
+        xcopy /e /v "dist" "../JPA_Pagination/src/main/resources/static"
+        cd ../JPA_Pagination
+        mvn package
+    ) else (
+        echo Error !!! building is not successful ... 
+        echo Ending deploy process... Bye~
+    )
+    
+) else (
+    echo we can't find pagination-example 
+    echo Ending deploy process... Bye~
+)
